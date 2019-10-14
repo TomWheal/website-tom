a0dd0b74b973ca3ba5b17565df67e80acd4d8fb8
Tom Wheal <tw1n15@soton.ac.uk>
2019-10-14 13:33:42 +0100

    Current Website

---
+++ js/bg.js
@@ -0,0 +1,30 @@
+/**
+ * Particleground demo
+ * @author Jonathan Nicol - @mrjnicol
+ */
+var delay=1000;
+
+document.addEventListener('DOMContentLoaded', function () {
+  setTimeout(function() {
+    particleground(document.getElementById('bg'), {
+    dotColor: '#a0a0a0',
+    lineColor: '#848484'
+  });
+  var intro = document.getElementById('footer');
+  intro.style.marginTop = - intro.offsetHeight / 2 + 'px';
+  }, delay);
+});
+
+
+/*
+// jQuery plugin example:
+$(document).ready(function() {
+  $('#particles').particleground({
+    dotColor: '#5cbdaa',
+    lineColor: '#5cbdaa'
+  });
+  $('.intro').css({
+    'margin-top': -($('.intro').height() / 2)
+  });
+});
+*/
